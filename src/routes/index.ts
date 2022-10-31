6847584058b1e32805b3a9e22028b44e1eb23346
singhayushh <coding.ayush@gmail.com>
2022-10-31 12:49:38 +0530

    home route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,9 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 
 const indexRouter: Router = Router();
+
+indexRouter.get("/", (req: Request, res: Response) => {
+    res.render("index");
+});
 
 export default indexRouter;
